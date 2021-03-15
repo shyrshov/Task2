6abdaf9b98b0dc4bc323f3cd8f2a2ccd53fd05ee
Andrii <a.shyrshov@gmail.com>
2021-03-16 01:35:25 +0200

    Initial commit

---
+++ src/com/company/Integers.java
@@ -0,0 +1,10 @@
+public class Integers {
+    public static void main (String [] args) {
+        int a = 2;
+        int b = 7;
+        System.out.println("Addition Result = " + (a + b));
+        System.out.println("Subtraction Result = " + (a - b));
+        System.out.println("Multiplication Result = " + (a * b));
+        System.out.println("Division Result = " + (a / b));
+    }
+}
