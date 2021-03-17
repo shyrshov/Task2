74388719c9765caec29145c3c986e204d141aaaa
Andrii <a.shyrshov@gmail.com>
2021-03-17 05:44:43 +0200

    divisible

---
+++ src/com/company/divisible.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class divisible {
+    public static void main (String[] args) {
+        for (int a = 1; a<=100; a++) {
+            if ((a % 3 == 0 || a % 5 ==0) && a % 2 != 0) {
+                System.out.println(a);
+            }
+        }
+    }
+}
